6b85ad231c02e9d16dcf73ab72e9bc46c1a38d9e
candicecz <czech.candice@gmail.com>
2017-09-24 00:33:50 -0400

    basic decision feature

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="startHere"><i className="material-icons">place</i> Start-Here</div>
         <Homepage />
       </div>
 
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,14 +1,145 @@
 import React, { Component } from 'react';
+import './index.css';
+import timeOfYear1 from './images/timeOfYear1.jpg';
+import timeOfYear2 from './images/timeOfYear2.jpg';
+import climate1 from './images/climate1.jpg';
+import climate2 from './images/climate2.jpg';
+import price1 from './images/price1.jpg';
+import price2 from './images/price2.jpg';
+import landscape1 from './images/landscape1.jpg';
+import landscape2 from './images/landscape2.jpg';
 
-import './index.css'
+var dimensions = {
+  timeOfYear: {
+    timeOfYear1: {
+      url:timeOfYear1,
+      value:'S',
+    },
+    timeOfYear2: {
+      url:timeOfYear2,
+      value:'W',
+    }
+  },
+  climate: {
+    climate1: {
+      url:climate1,
+      value:'H',
+    },
+    climate2: {
+      url:climate2,
+      value:'C',
+    }
+  },
+  price: {
+    price1: {
+      url:price1,
+      value:'S',
+    },
+    price2: {
+      url:price2,
+      value:'I',
+    }
+  },
+  landscape: {
+    landscape1: {
+      url:landscape1,
+      value:'C',
+    },
+    landscape2: {
+      url:landscape2,
+      value:'N',
+    }
+  }
+}
+
+
 
 class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userChoice: {
+        timeOfYear: null,
+        climate: null
+      },
+      currentChoice: {
+        promptMessage:"When would you like to leave?",
+        option1:dimensions.timeOfYear.timeOfYear1,
+        option2:dimensions.timeOfYear.timeOfYear2,
+        dimension:'timeOfYear'
+      }
+    }
+  }
+
+  _handleClick = (selectedOption) => {
+    let {currentChoice} = this.state
+    if(currentChoice.dimension === 'timeOfYear') {
+      this.setState({
+        userChoice: {
+          timeOfYear: selectedOption
+        },
+        currentChoice: {
+          promptMessage:"Are you looking for hot or cold?",
+          option1:dimensions.climate.climate1,
+          option2:dimensions.climate.climate2,
+          dimension:'climate'
+        }
+      })
+    }
+    else if(currentChoice.dimension === 'climate') {
+      this.setState({
+        userChoice: {
+          climate: selectedOption
+        },
+        currentChoice: {
+          promptMessage:"Is your budget big or small?",
+          option1:dimensions.price.price1,
+          option2:dimensions.price.price2,
+          dimension:'price'
+        }
+      })
+    }
+    else if(currentChoice.dimension === 'price') {
+      this.setState({
+        userChoice: {
+          price: selectedOption
+        },
+        currentChoice: {
+          promptMessage:"Do you prefer an urban setting or a rural one?",
+          option1:dimensions.landscape.landscape1,
+          option2:dimensions.landscape.landscape2,
+          dimension:'landscape'
+        }
+      })
+    }
+    else if(currentChoice.dimension === 'landscape') {
+      this.setState({
+        userChoice: {
+          landscape: selectedOption
+        },
+        currentChoice: {
+          promptMessage:"We suggest:",
+          option1:dimensions.landscape.landscape1,
+          option2:dimensions.landscape.landscape2,
+          dimension:'landscape'
+        }
+      })
+    }
+  }
+
+
   render() {
+    let {currentChoice} = this.state
     return (
-        <div className="container">
-          <div className="box1 col-small-6">sdafsadfsa</div>
-          <div className="box2 col-small-6"></div>
+      <div>
+        <div className="imageSection">
+          <div className="prompt">{currentChoice.promptMessage}</div>
+          <div className="row">
+              <img className="box col-small-6" src={currentChoice.option1.url} onClick={() => this._handleClick(currentChoice.option1.value)} alt="option1"/>
+              <img className="box col-small-6"  src={currentChoice.option2.url} onClick={() => this._handleClick(currentChoice.option2.value)} alt="option2"/>
+          </div>
         </div>
+      </div>
     );
   }
 }
