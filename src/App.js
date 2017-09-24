37863661aff1b521f26f451f2b26f334bc2d2876
Tim McNamara <timlmcn@gmail.com>
2017-09-24 15:04:00 -0400

    logo and color

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Homepage from './Homepage';
 import './index.css'
 import logo from './images/logo.png';
+import logo2 from './images/logo2.png';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="startHere"><img className="logo" src={logo} alt="logo"/>Start Here</div>
+        <div className="startHere"><img className="logo" src={logo2} alt="logo"/></div>
         <Homepage />
       </div>
 
